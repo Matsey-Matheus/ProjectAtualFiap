61caacc9715a2bd07b240416f9fdb9969d5d4e69
Matheus <mwrsgames1@gmai.com>
2022-04-26 18:24:14 -0300

    Att Estruturas repetição.

---
+++ TokioFiapMatheus/src/logica/aulas/aula06/EstruturasRepeticao.java
@@ -0,0 +1,21 @@
+package logica.aulas.aula06;
+
+import java.util.Scanner;
+
+public class EstruturasRepeticao {
+
+	public static void main(String[] args) {
+		
+		Scanner tec = new Scanner(System.in);
+		
+		int num, soma = 0;
+		
+		System.out.print("Digite um numero: ");
+		num = tec.nextInt();
+		soma = soma + num;
+		System.out.println(soma);
+		
+
+	}
+
+}
